3331e2651f59ed2e5d56604c64b83b8096bc4725
Zachary Cutlip <uid000@gmail.com>
2020-09-25 15:39:33 -0700

    fix __init__.py to accomodate reorganization

--- pyonepassword/__init__.py
+++ pyonepassword/__init__.py
@@ -1,11 +1,12 @@
-from .__about__ import (
+from .__about__ import (  # noqa: F401
     __version__,
     __title__,
     __summary__
 )
 
-from .pyonepassword import (   # noqa: F401
-    OP,
+from .pyonepassword import OP  # noqa: F401
+
+from .py_op_exceptions import (  # noqa: F401
     OPLookupException,
     OPSigninException,
     OPNotFoundException,
